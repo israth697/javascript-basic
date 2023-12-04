cb26bd027bca775d01bd94f74c29d1a81322e0a8
israth697 <israth.jarin.23@gmail.com>
2023-12-04 17:40:11 +0600

    for loop

---
+++ .js
@@ -0,0 +1,13 @@
+//forloop
+
+for (var i = 0; i < 10; i++) {
+    console.log(i + ": " + i*3 + "<br />");
+    }
+    var sum = 0;
+    var a=[1,2,3,5];
+    for (var i = 0; i < a.length; i++) {
+    sum +=a[i];
+    }               // parsing an array
+    html = "";
+    
+
