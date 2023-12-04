f9058d7e1a351d61bd8f6e61ddcaae89bf6236c1
israth697 <israth.jarin.23@gmail.com>
2023-12-04 17:51:57 +0600

    while loop

--- .js
+++ .js
@@ -1,13 +1,23 @@
 //forloop
 
-for (var i = 0; i < 10; i++) {
-    console.log(i + ": " + i*3 + "<br />");
-    }
-    var sum = 0;
-    var a=[1,2,3,5];
-    for (var i = 0; i < a.length; i++) {
-    sum +=a[i];
-    }               // parsing an array
-    html = "";
+// for (var i = 0; i < 10; i++) {
+//     console.log(i + ": " + i*3 + "<br />");
+//     }
+//     var sum = 0;
+//     var a=[1,2,3,5];
+//     for (var i = 0; i < a.length; i++) {
+//     sum +=a[i];
+//     }               // parsing an array
+//     html = "";
+
+var i = 1;                      // initialize
+while (i < 100) {               // enters the cycle if statement is true
+i *= 2;                     // increment to avoid infinite loop
+document.write(i + ", ");   // output
+}
+
+
+// var a=['moneyplant','top','stone','roses','fruits'];
+// console.log(a[1]);
     
 
